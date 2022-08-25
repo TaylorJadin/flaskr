4b78edaa5a15762c51b6ef6475cbcf3bfc5896b8
Taylor Jadin <jadin.taylor@gmail.com>
2022-08-24 23:23:38 -0500

    path

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ import sys
 path = '/var/www/webroot/ROOT'
 if path not in sys.path:
     sys.path.append(path)
-from flaskr.__init__.py import app
+from flaskr.__init__ import app
 application = app
